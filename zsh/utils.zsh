5e2c9ae36f7185307fc52d9c452ee555601ad731
Marley Reeves <marley@marlstar.com>
2025-01-09 15:39:08 +1000

    add aliases for zellij (`zj=zellij`) and git (`gitgraph=git log --graph --oneline --color`)

--- zsh/utils.zsh
+++ zsh/utils.zsh
@@ -1,2 +1,9 @@
+# Git
+alias gitgraph="git log --graph --oneline --color"
+
+# Zellij
+alias zj="zellij"
+
+# Robotics stuff
 alias "robotc"="adb connect 192.168.43.1"
 alias "robotd"="adb disconnect"
