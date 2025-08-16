ec3d347bd9e2015f56209cddf38c6ae3b10118a6
Marley Reeves <marley@marlstar.com>
2025-08-16 15:44:00 +1000

    fish: chafa

--- fish/.config/fish/functions/chafa.fish
+++ fish/.config/fish/functions/chafa.fish
@@ -1,4 +1,8 @@
 # Make chafa work with tmux without having to manually specify flags
 function chafa
-	chafa -f kitty --passthrough tmux
+	if set -q TMUX
+		command chafa -f kitty --passthrough tmux $argv
+	else
+		command chafa -f kitty $argv
+	end
 end
